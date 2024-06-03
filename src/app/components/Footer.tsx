4efbf16c0c881b43c002a9c2b0b3b7ea7907d26c
Tatiana <tatigerzens@gmail.com>
2024-06-03 16:51:23 -0300

    added footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='footer container mx-auto top-0 left-0 right-0 z-10'>
+      <div className='flex p-12 justify-between text-[#626577]'>
+        <span>LOGO</span>
+        <p className='text-[#626577]'>All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
